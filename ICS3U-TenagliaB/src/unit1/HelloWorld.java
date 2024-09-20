3c4fbc6758f4dcb531ebb7a4c7fd83090fdc4dd7
342846805 <342846805@C-MXL2393F1Z.tdsb.on.ca>
2024-09-20 11:11:20 -0400

    Adding ICS3U project with HelloWorld class

---
+++ ICS3U-TenagliaB/src/unit1/HelloWorld.java
@@ -0,0 +1,20 @@
+package unit1;
+/**
+ * Description: Prints two text lines <br>
+ * Date: September 19, 2024
+ * @author Ben Tenaglia
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//this prints text to the console
+		System.out.println("Hello world.");
+		System.out.println("The names Tenaglia, Ben Tenaglia.");
+		
+		/*
+		 * this is a block comment
+		 * you can add notes on multiple lines
+		 */
+	}
+
+}
