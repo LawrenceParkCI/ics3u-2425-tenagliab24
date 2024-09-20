06bf4324b20a91f41ee0dd5ec9b49382b44ddc0e
342846805 <342846805@C-MXL2393F1Z.tdsb.on.ca>
2024-09-20 11:38:08 -0400

    Adding ICS3U project with AboutMe class

---
+++ ICS3U-TenagliaB/src/unit1/AboutMe.java
@@ -0,0 +1,19 @@
+package unit1;
+/**
+ * Description: Prints my name, Grade, Teacher and School <br>
+ * Date: September 20, 2024
+ * @author Ben Tenaglia
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		//this prints the text to the console
+		System.out.println("-------------------------------------------------------------------");
+		System.out.println("About me!");
+		System.out.println("- My Name is Ben Tenaglia and I am in Grade 11 at Lawrence Park CI.");
+		System.out.println("- My Teacher for this class is Ms.Kemp");
+		System.out.println("-------------------------------------------------------------------");
+		
+	}
+
+}
