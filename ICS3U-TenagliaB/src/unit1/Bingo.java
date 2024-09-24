d0da60bf7d2521346a93c505c591ba03db314a2b
342846805 <342846805@C-MXL2393F1Z.tdsb.on.ca>
2024-09-24 11:38:50 -0400

    Started work on Bingo project, added the rules, and the start of the bingo board

---
+++ ICS3U-TenagliaB/src/unit1/Bingo.java
@@ -0,0 +1,16 @@
+package unit1;
+
+public class Bingo {
+
+	public static void main(String[] args) {
+		System.out.println("Bingo Card");
+		System.out.println("\n1. The caller randomly pulls a numbered bingo ball.");
+		System.out.println("\n2. The number is placed on the bingo board and called out.");
+		System.out.println("\n3. Players look for the called number on their bingo card.");
+		System.out.println("\n4. If the number is located, it is marked off.");
+		System.out.println("\n5. Steps 1 and 4 are repeated until a player matches the BINGO pattern.");
+		System.out.println("\n6. The winning player yells BINGO!");
+		System.out.format("\n\t\tB\tI\tN\tG\tO");
+	}
+
+}
