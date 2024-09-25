999004ef12be750b3bcdb2b9503526f2f01f2328
342846805 <342846805@C-MXL2393F1Z.tdsb.on.ca>
2024-09-25 10:17:17 -0400

    Coded a Bingo board with random numbers and a free space.

--- ICS3U-TenagliaB/src/unit1/Bingo.java
+++ ICS3U-TenagliaB/src/unit1/Bingo.java
@@ -1,8 +1,13 @@
 package unit1;
-
+/**
+ * Description: Prints rules for the game "BINGO" and also prints the rules above.
+ * <p> Date: September 25, 2024
+ * @author Ben Tenaglia
+ */
 public class Bingo {
 
 	public static void main(String[] args) {
+		//This prints the Bingo Rules
 		System.out.println("Bingo Card");
 		System.out.println("\n1. The caller randomly pulls a numbered bingo ball.");
 		System.out.println("\n2. The number is placed on the bingo board and called out.");
@@ -10,7 +15,13 @@ public class Bingo {
 		System.out.println("\n4. If the number is located, it is marked off.");
 		System.out.println("\n5. Steps 1 and 4 are repeated until a player matches the BINGO pattern.");
 		System.out.println("\n6. The winning player yells BINGO!");
-		System.out.format("\n\t\tB\tI\tN\tG\tO");
+		//This prints the Bingo board
+		System.out.format("\n\t\tB\tI\tN\tG\tO\n");
+		System.out.format("\n\t\t9\t23\t34\t50\t70\n");
+		System.out.format("\n\t\t6\t29\t44\t46\t62\n");
+		System.out.format("\n\t\t8\t18     FREE\t59\t75\n");
+		System.out.format("\n\t\t14\t22\t40\t55\t68\n");
+		System.out.format("\n\t\t2\t28\t37\t60\t69\n");
 	}
 
 }
