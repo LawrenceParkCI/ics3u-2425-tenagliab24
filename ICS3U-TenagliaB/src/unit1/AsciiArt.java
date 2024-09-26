e8d5758a33aac73055df6e9b454c0e4624250aba
342846805 <342846805@C-MXL2393F1Z.tdsb.on.ca>
2024-09-26 10:46:22 -0400

    Used an Ascii art generator to create my name, and put it into code.

---
+++ ICS3U-TenagliaB/src/unit1/AsciiArt.java
@@ -0,0 +1,21 @@
+/**
+ * Description: This code prints "Ascii art" in the form of a signature
+ * Date: September 24, 2024
+ * @author Ben Tenaglia
+ */
+package unit1;
+
+public class AsciiArt {
+
+	public static void main(String[] args) {
+		System.out.println("   ____  U _____ u _   _          _____  U _____ u _   _       _       ____     _                    _      ");
+		System.out.println("U | __\")u\\| ___\"|/| \\ |\"|        |_ \" _| \\| ___\"|/| \\ |\"|  U  /\"\\  uU /\"___|u  |\"|        ___    U  /\"\\  u  ");
+		System.out.println(" \\|  _ \\/ |  _|\" <|  \\| |>         | |    |  _|\" <|  \\| |>  \\/ _ \\/ \\| |  _ /U | | u     |_\"_|    \\/ _ \\/   ");
+		System.out.println("  | |_) | | |___ U| |\\  |u        /| |\\   | |___ U| |\\  |u  / ___ \\  | |_| |  \\| |/__     | |     / ___ \\   ");
+		System.out.println("  |____/  |_____| |_| \\_|        u |_|U   |_____| |_| \\_|  /_/   \\_\\  \\____|   |_____|  U/| |\\u  /_/   \\_\\  ");
+		System.out.println(" _|| \\\\_  <<   >> ||   \\\\,-.     _// \\\\_  <<   >> ||   \\\\,-.\\\\    >>  _)(|_    //  \\\\.-,_|___|_,-.\\\\    >>  ");
+		System.out.println("(__) (__)(__) (__)(_\")  (_/     (__) (__)(__) (__)(_\")  (_/(__)  (__)(__)__)  (_\")(\"_)\\_)-' '-(_/(__)  (__) ");
+
+	}
+
+}
