d20b1610d0d7f5bbf6bace48e24e40d0a3f76a78
342846805 <342846805@C-MXL2393F1Z.tdsb.on.ca>
2024-10-02 11:40:21 -0400

    Completing the tasks listed on the String Variables and Integer Variables worksheet

---
+++ ICS3U-TenagliaB/src/unit1/RectangleArea.java
@@ -0,0 +1,36 @@
+package unit1;
+/**
+ * Description: Completed the tasks for the Integer variables worksheet <p>
+ * Date: October 2, 2024
+ * @author Ben Tenaglia
+ */
+import java.util.Scanner;
+
+public class RectangleArea {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//declare variables
+		int length, width, depth;
+		
+		//get the user input 
+		System.out.println("AREA PROGRAM");
+		System.out.println("Type in the length of the rectangle and press <Enter>: ");
+		length = sc.nextInt();
+		
+		System.out.println("Type in the width of the rectangle and press <Enter>: ");
+		width = sc.nextInt();
+		
+		System.out.println("Type in the depth of the rectangle and press <Enter>: ");
+		depth = sc.nextInt();
+		
+		//calculate the volume
+		int volume = length * width * depth;
+		
+		//print the output
+		System.out.println("The volume of your rectangle is " + volume);
+		sc.close();
+	}
+
+}
