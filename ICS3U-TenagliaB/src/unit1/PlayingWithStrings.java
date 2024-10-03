4a21bdccbb0c31d8072e186e6658c27a93113cba
342846805 <342846805@C-MXL2393F1Z.tdsb.on.ca>
2024-10-03 10:15:47 -0400

    got to question F on the final worksheet

---
+++ ICS3U-TenagliaB/src/unit1/PlayingWithStrings.java
@@ -0,0 +1,10 @@
+package unit1;
+
+public class PlayingWithStrings {
+
+	public static void main(String[] args) {
+		String name = "Runnymede";
+		System.out.println(name.length() + "Ravens".length());
+	}
+
+}
